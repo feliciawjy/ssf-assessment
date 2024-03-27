1c80ed9b0e47656357adb35f2ad0754560d7a4dc
feliciawjy <feliciawjy@gmail.com>
2024-03-27 11:40:58 +0800

    task 7 done

--- src/main/java/sg/edu/nus/iss/ibfb4ssfassessment/controller/LoginController.java
+++ src/main/java/sg/edu/nus/iss/ibfb4ssfassessment/controller/LoginController.java
@@ -8,26 +8,40 @@ import jakarta.servlet.http.HttpSession;
 import jakarta.validation.Valid;
 import sg.edu.nus.iss.ibfb4ssfassessment.model.Login;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class LoginController {
-    
-
 
     // TODO: Task 6
-    public String login() {
-        return null;
+    @GetMapping(path = { "/", "/index.html", "/login" })
+    public String login(Model model) {
+        model.addAttribute("login", new Login());
+        return "view0";
 
     }
 
     // TODO: Task 7
-    public String processlogin() {
-        return null;
-
-
+    @PostMapping(path = "/login")
+    public ModelAndView processLogin(HttpSession session, @ModelAttribute("login") @Valid Login login,
+            BindingResult bindings) {
+        ModelAndView mav = new ModelAndView();
+        mav.addObject(login);
+        if (bindings.hasErrors()) {
+            mav.addObject(login);
+            mav.setViewName("view0");
+            System.out.println("Global error: " + bindings.getGlobalErrors());
+            System.out.println("Field error:" + bindings.getFieldErrors());
+        } else {
+            mav.setViewName("view1"); // redirect?
+            session.setAttribute("login", login);
+            System.out.println(login.toString());
+        }
+        return mav;
     }
-    
 
     // For the logout button shown on View 2
     // On logout, session should be cleared
@@ -35,5 +49,5 @@ public class LoginController {
         return null;
 
     }
-    
+
 }
