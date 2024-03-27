a7f2323e1e38750fec9b9a1cd0032fa7f05c5460
feliciawjy <feliciawjy@gmail.com>
2024-03-27 12:22:18 +0800

    task 8 done

--- src/main/java/sg/edu/nus/iss/ibfb4ssfassessment/controller/MovieController.java
+++ src/main/java/sg/edu/nus/iss/ibfb4ssfassessment/controller/MovieController.java
@@ -1,30 +1,42 @@
 package sg.edu.nus.iss.ibfb4ssfassessment.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import jakarta.servlet.http.HttpSession;
+import sg.edu.nus.iss.ibfb4ssfassessment.model.Movie;
+import sg.edu.nus.iss.ibfb4ssfassessment.service.DatabaseService;
 
 @Controller
+@RequestMapping("/movie")
 public class MovieController {
 
-
+    @Autowired
+    DatabaseService databaseService;
 
     // TODO: Task 8
-    public String displayMovies() {
-        return null;
-
+    @GetMapping("/list")
+    public String displayMovies(Model model, HttpSession httpSession) {
+        if (null != httpSession.getAttribute("login")) {
+            List<Movie> movies = databaseService.getAllMovies();
+            model.addAttribute("movies", movies);
+            return "view2";
+        } else {
+            return "redirect:/login";
+        }
 
     }
 
     // TODO: Task 9
-    public String bookMovie()  {
+    public String bookMovie() {
         return null;
-
 
     }
 
